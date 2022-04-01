863f5fe655df50937c0c7abb8e4c40456d7dde30
Harithahari16 <74651763+Harithahari16@users.noreply.github.com>
2022-04-01 12:00:30 +0530

    Add files via upload

---
+++ DSLab/array_2d.py
@@ -0,0 +1,21 @@
+import numpy as np
+x = np.array([[1,2],[5,6]])
+y = np.array([[8,7],[3,4]])             
+print('First 2D Array:')
+print(x)
+print('Second 2D Array:')
+print(y)
+
+print('Transpose of matrix:')
+print(x.transpose())
+print(y.transpose())
+
+print('Matrix Multiplication:')
+print(np.dot(x,y))
+
+
+
+
+
+
+
